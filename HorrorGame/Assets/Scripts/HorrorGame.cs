c159b2ea071a3545f269a2221889818ca05d9ee3
huynino96 <huynino96@gmail.com>
2021-03-09 11:31:43 +0700

    Manage More State

---
+++ HorrorGame/Assets/Scripts/HorrorGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HorrorGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    State state;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+    private void ManageState()
+    {
+        var nextStates = state.GetNextState();
+        
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+                Debug.Log(state);
+               
+            }
+            else if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                state = nextStates[nextStates.Length - 1];
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            state = startingState;
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ HorrorGame/Assets/Scripts/State.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Story")]
+public class State : ScriptableObject
+{
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextState;
+    [SerializeField] List<State> previousState = new List<State>();
+
+    // Start is called before the first frame update
+    public string GetStateStory()
+    {
+        return storyText;
+    } 
+
+    public List<State> GetPreviousState()
+    {
+        return previousState;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextState;
+    }
+
+}
